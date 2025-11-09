ea7fb4b61ae61a3004801547648a9f13e6edcb9c
3plus1wheels <3plus1wheels@gmail.com>
2025-11-08 18:04:35 -0700

    Co-authored-by: Hoang Bach Phan <bachphan05@users.noreply.github.com>

--- backend/api/models.py
+++ backend/api/models.py
@@ -12,6 +12,7 @@ class Neighborhood(models.Model):
     name = models.CharField(max_length=200, unique=True)
     city = models.CharField(max_length=100)
     country = models.CharField(max_length=100)
+    zip_code = models.CharField(max_length=20, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     def __str__(self):
         return self.name
